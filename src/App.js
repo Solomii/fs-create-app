3fb5eb244a9a738f2e44330dca3c3a52e3cc9c94
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-03-01 14:35:21 +0200

    Add PropTypes and JSDoc to component Ciao

--- src/App.js
+++ src/App.js
@@ -4,16 +4,27 @@ import "./App.css";
 // import CiaolSection from "./components/CiaoSection/index";
 // import StopWatch from './components/StopWatch';
 // import UserSection from './components/UserSection';
-import FormClass from './components/forms2/FormClass/index';
+// import FormClass from './components/forms2/FormClass/index';
+import Ciao from "./components/Ciao";
 
 const App = (props) => {
+    const user = {
+        id: 1,
+        firstName: "Tom",
+        lastName: "Test",
+    };
     return (
         <>
             {/* <CiaolSection />
             <StopWatch/>
             <UserSection/> */}
             {/* <SignInFrom/> */}
-            <FormClass/>
+            {/* <FormClass/> */}
+            <Ciao
+                classStyle={"ciao__welcome"}
+                name={user.firstName}
+                id={user.id}
+            />
         </>
     );
 };
--- src/components/Ciao/index.js
+++ src/components/Ciao/index.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
 import CiaoButton from "../CiaoButton";
+
+  /**
+     * @param {*} props 
+     * @param {string} props.classStyle 
+     * @param {string} props.name
+     * @param {number} props.id
+     */
 
 class Ciao extends Component {
     constructor(props) {
@@ -19,10 +27,13 @@ class Ciao extends Component {
         const { isHi } = this.state;
         const { classStyle, name, id } = this.props;
 
+        //погана практика, не робити так!!!
+        // if(typeof name !== "string")
+
         return (
             <div className={classStyle}>
                 <h2>
-                    id:{id}
+                    id:{id})
                     <br></br>
                     {isHi ? "Hi" : "Bye"} {name ?? "anonim"}{" "}
                 </h2>
@@ -31,5 +42,11 @@ class Ciao extends Component {
         );
     }
 }
+
+Ciao.propTypes = {
+    classStyle: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    id:PropTypes.number.isRequired
+  };
 
 export default Ciao;
