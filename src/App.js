d48efd34f2945d6a1dab28a4b5eb234e763d38f3
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-03-02 16:45:41 +0200

    Add Sass to stopWatch

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Calendar from "./components/Calendar";
 // import HeandingH1 from "./components/HeadingH1";
 // import SignInFrom from "./components/forms/SingInForm";
 // import CiaolSection from "./components/CiaoSection/index";
-// import StopWatch from './components/StopWatch';
+import StopWatch from './components/StopWatch';
 // import UserSection from './components/UserSection';
 // import FormClass from './components/forms2/FormClass/index';
-// import Ciao from "./components/Ciao";
+import Ciao from "./components/Ciao";
 // import UserCard from "./components/UserSection/UserCard";
 // import UserList from './components/UserSection/UserList/index';
 
@@ -30,18 +30,18 @@ const App = (props) => {
         <>
           {/* <HeandingH1 /> */}
             {/* <CiaolSection /> */}
-            {/* <StopWatch/> */}
+            <StopWatch/>
             {/* <UserSection/>  */}
             {/* <SignInFrom/> */}
             {/* <FormClass/> */}
-            {/* <Ciao
-                classStyle={"ciao__welcome"}
-                name={user.firstName}
-                id={user.id}
-            /> */}
             {/* <UserCard user={user}/> */}
             {/* <UserList users={users}/> */}
             <Calendar/>
+            <Ciao
+                classStyle={"ciao__welcome"}
+                name="Tom"
+                id={1}
+            />
         </>
     );
 };
--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
 import { format,add,startOfMonth } from "date-fns";
+import styles from  "./Calendat.module.scss"
 
 class Calendar extends Component {
     constructor(props) {
@@ -21,16 +22,14 @@ class Calendar extends Component {
     render() {
         const { date } = this.state;
         return (
-            <div>
-               Calenday
-            
-               <p>  день тажня з якого почався місяць: {format(startOfMonth(date), "EEEE" )}
+            <div className={styles.box}>
+               <p className={styles.content}>  день тажня з якого почався місяць: {format(startOfMonth(date), "EEEE" )}
                </p>
-                <p>
+                <p className={styles.content}>
                     {format(date, "'Date:' EEEE, dd LLLL 'Time:' HH:mm:ss")}
                 </p>
-                <button onClick={this.addDay}>add 1 day</button>
-                <button onClick={this.addMonth}>add 1 Month</button>
+                <button className={styles.btn} onClick={this.addDay}>add 1 day</button>
+                <button className={styles.btn}  onClick={this.addMonth}>add 1 Month</button>
             </div>
         );
     }
